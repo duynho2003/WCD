6c6adf1f1fa7a56cbfc17f85db3de6fb47781718
Tommy Nguyen <khenchaynguyen@gmail.com>
2024-02-29 22:35:24 +0700

    up Student1408049

---
+++ Student1408049/src/main/java/com/mytech/student1408049/helpers/ServletHelper.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mytech.student1408049.helpers;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ *
+ * @author caova
+ */
+public class ServletHelper {
+    public static void forward(HttpServletRequest request, HttpServletResponse response, String page) throws ServletException, IOException {
+        request.getRequestDispatcher("/WEB-INF/" + page + ".jsp").forward(request, response);
+    }
+    
+    public static void redirect(HttpServletRequest request, HttpServletResponse response, String page) throws ServletException, IOException {
+        System.out.println("Redirect context path: " + request.getContextPath());
+        response.sendRedirect(request.getContextPath() + "/" + page);
+    }
+}
